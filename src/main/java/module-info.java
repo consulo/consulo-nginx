3b04fc823c2d4477203a4546d8f6255695bfc8fc
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 17:02:50 +0300

    drop ide-api usage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,37 @@
 module consulo.nginx {
     requires consulo.ide.api;
 
+    requires consulo.application.api;
+    requires consulo.application.content.api;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.component.api;
+    requires consulo.configurable.api;
+    requires consulo.disposer.api;
+    requires consulo.document.api;
+    requires consulo.execution.api;
+    requires consulo.execution.impl;
+    requires consulo.file.chooser.api;
+    requires consulo.language.api;
+    requires consulo.language.impl;
+    requires consulo.language.editor.api;
+    requires consulo.language.code.style.api;
+    requires consulo.localize.api;
+    requires consulo.logging.api;
+    requires consulo.navigation.api;
+    requires consulo.platform.api;
+    requires consulo.process.api;
+    requires consulo.project.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.ui.ex.awt.api;
+    requires consulo.virtual.file.system.api;
+    requires consulo.util.collection;
+    requires consulo.util.dataholder;
+    requires consulo.util.io;
+    requires consulo.util.lang;
+    requires consulo.util.xml.serializer;
+
     // TODO [VISTALL] remove it in future
     requires java.desktop;
     requires forms.rt;
